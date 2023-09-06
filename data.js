b930a21b2df63830a4a8a2653410bec0444f173d
Maxim <ionmax1311@gmail.com>
2023-09-06 17:20:11 +0200

    first-commit

---
+++ data.js
@@ -0,0 +1,413 @@
+let data = {
+    // PRODUCT_DESIGNER: {
+    //   id: 1,
+    //   icon: "./img/des-icon.svg",
+    //   position: "Product (UI/UX) Designer",
+    //   description: "",
+    //   experience: "4-5 лет",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "PRODUCT_DESIGNER",
+    //   closeVac: true,
+    //   visibility: false,
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         "Разработка дизайна и дизайн-системы;",
+    //         "Создание прототипов/вайрфреймов;",
+    //         "Тестирование интерфейсов;",
+    //         "Изучение и внедрение практик UI/UX;",
+    //         "Сотрудничество с PO и BA.",
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         "2+ года опыта в создании IT продуктов;",
+    //         "Опыт в  разработке дизайн-систем с нуля;",
+    //         "Понимание принципов UI/UX;",
+    //         "Проактивность, инициативность, ответственность;",
+    //         "Знание современных трендов в дизайне.",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         "Наличие портфолио на Behance/Dribbble;",
+    //         "Знание рынка аффилейт/рекламных сетей;",
+    //         "Опыт работы с Enterpirse/SaaS продуктами.",
+    //       ],
+    //     },
+    //   },
+    // },
+    // DEV_OPS: {
+    //   id: 2,
+    //   icon: "./img/devops-icon.svg",
+    //   position: "Devops Engineer",
+    //   description: "",
+    //   experience: "3-4 года",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "DEV_OPS",
+    //   closeVac: true,
+    //   visibility: false,
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         "Мониторинг/управление UNIX серверами;",
+    //         "Менеджмент веб-серверов apache/nginx;",
+    //         "Сопровождение Gitlab CI/CD;",
+    //         "Написание bash скриптов, Ansible.",
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         "Понимание TCP/IP-сетей, HTTP/HTTPS протоколов, DNS,SMTP;",
+    //         "Понимание работы РСУБД (PostgreSQL, MySQL);",
+    //         "Знание GIT и git сервисов, gitlab;",
+    //         "Опыт настройки веб-серверов Apache, Nginx.",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         "Знание стека EFK/ELK.",
+    //       ],
+    //     },
+    //   },
+    // },
+    // FRONTEND_LEAD: {
+    //   id: 3,
+    //   icon: "./img/js-icon.svg",
+    //   position: "Front End Team Lead (VUE)",
+    //   description: "",
+    //   experience: "4-5 лет",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "FRONTEND_LEAD",
+    //   closeVac: true,
+    //   visibility: false,
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         "Активное участие в проектировании системы;",
+    //         "Разработка новых фронтенд сервисов;",
+    //         "Набор сотрудников команды разработки;",
+    //         "Менторство сотрудников команды разработки;",
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         "3+ года опыта разработки коммерческих веб приложений;",
+    //         "2+ года работы с фреймворком Vue;",
+    //         "Навыки в разработке адаптивных макетов;",
+    //         "Опыт в менеджменте и управлении командой;",
+    //         "Лидерские качества;",
+    //         "Стрессоустойчивость.",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         "Опыт разработки дизайн-систем;",
+    //         "Знание рынка аффилейт/рекламных сетей;",
+    //         "Опыт работы с Enterpirse/SaaS продуктами.",
+    //       ],
+    //     },
+    //   },
+    // },
+    FRONTEND_DEV: {
+        id: 4,
+        icon: "./img/vue-icon.svg",
+        position: "frontendDev.position",
+        description: "frontendDev.description",
+        experience: "frontendDev.experience",
+        officeLocation: "frontendDev.officeAdress",
+        collaborationVariant: "frontendDev.office",
+        buttonData: "FRONTEND_DEV",
+        closeVac: false,
+        visibility: true,
+        inside: {
+            paragraph1: {
+                title: "frontendDev.paragraph1.title",
+                p: [
+                    "frontendDev.paragraph1.1",
+                    "frontendDev.paragraph1.2",
+                    "frontendDev.paragraph1.3",
+                    "frontendDev.paragraph1.4",
+                    "frontendDev.paragraph1.5",
+                    "frontendDev.paragraph1.6",
+                    "frontendDev.paragraph1.7",
+                    "frontendDev.paragraph1.8",
+                    "frontendDev.paragraph1.9",
+                    "frontendDev.paragraph1.10",
+                    "frontendDev.paragraph1.11",
+                ],
+            },
+            paragraph2: {
+                title: "frontendDev.paragraph2.title",
+                p: [
+                    "frontendDev.paragraph2.1",
+                    "frontendDev.paragraph2.2",
+                    "frontendDev.paragraph2.3",
+                    "frontendDev.paragraph2.4",
+                    "frontendDev.paragraph2.5",
+                    "frontendDev.paragraph2.6",
+                    "frontendDev.paragraph2.7",
+                ],
+            },
+            paragraph3: {
+                title: "frontendDev.paragraph3.title",
+                p: [
+                    "frontendDev.paragraph3.1",
+                    "frontendDev.paragraph3.2",
+                    "frontendDev.paragraph3.3",
+                    "frontendDev.paragraph3.4",
+                    "frontendDev.paragraph3.5",
+                ],
+            },
+        },
+    },
+
+    PYTHON_DEV: {
+        id: 5,
+        icon: "./img/python-icon.png",
+        position: "pythonDev.position",
+        description: "pythonDev.description",
+        experience: "pythonDev.experience",
+        officeLocation: "pythonDev.officeAdress",
+        collaborationVariant: "pythonDev.office",
+        buttonData: "PYTHON_DEV",
+        closeVac: false,
+        visibility: true,
+        inside: {
+            paragraph1: {
+                title: "pythonDev.paragraph1.title",
+                p: [
+                    "pythonDev.paragraph1.1",
+                    "pythonDev.paragraph1.2",
+                    "pythonDev.paragraph1.3",
+                    "pythonDev.paragraph1.4",
+                    "pythonDev.paragraph1.5",
+                    "pythonDev.paragraph1.6",
+                    "pythonDev.paragraph1.7",
+                    "pythonDev.paragraph1.8",
+                    "pythonDev.paragraph1.9",
+                    "pythonDev.paragraph1.10",
+                    "pythonDev.paragraph1.11",
+                ],
+            },
+            paragraph2: {
+                title: "pythonDev.paragraph2.title",
+                p: [
+                    "pythonDev.paragraph2.1",
+                    "pythonDev.paragraph2.2",
+                    "pythonDev.paragraph2.3",
+                    "pythonDev.paragraph2.4",
+                    "pythonDev.paragraph2.5",
+                    "pythonDev.paragraph2.6",
+                ],
+            },
+            paragraph3: {
+                title: "pythonDev.paragraph3.title",
+                p: [
+                    "pythonDev.paragraph3.1",
+                    "pythonDev.paragraph3.2",
+                    "pythonDev.paragraph3.3",
+                    "pythonDev.paragraph3.4",
+                ],
+            },
+        },
+    },
+
+    // AUTOMATION_QA: {
+    //   id: 5,
+    //   icon: "./img/new-qa-icon.svg",
+    //   position: "AUTOMATION QA",
+    //   description: "",
+    //   experience: "3-4 года",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "AUTOMATION_QA",
+    //   closeVac: false,
+    //   visibility: false,
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         "Организация автоматизированного тестирования;",
+    //         "Тестирование API, веб-сервисов;",
+    //         "Разработка документации тестирования;",
+    //         "Заведение и оформление багов.",
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         "3+ года опыта в тестировании коммерческих приложений;",
+    //         "1+ год в автоматизированном тестировании;",
+    //         "Опыт и организация работы в Postman;",
+    //         "Навыки написания структурированных тестовых сценариев;",
+    //         "Проактивность.",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         "Опыт в нагрузочном тестировании;",
+    //         "Опыт в автоматизации UI тестов;",
+    //         "Работа с Jira/Confluence;",
+    //         "Знание английского языка.",
+    //       ],
+    //     },
+    //   },
+    // },
+
+    // MANUAL_QA: {
+    //   id: 6,
+    //   icon: "./img/new-qa-icon.svg",
+    //   position: "MANUAL QA",
+    //   description: "",
+    //   experience: "2-3 года",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "MANUAL_QA",
+    //   closeVac: false,
+    //   visibility: true,
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         "Тестирование WEB/API приложения;",
+    //         "Разработка документации тестирования;",
+    //         "Заведение и оформление багов;",
+    //         "Написание тест-кейсов.",
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         "2+ года опыта в тестировании коммерческих приложений;",
+    //         "Опыт работы с тестированием API;",
+    //         "Навыки написания структурированных тестовых сценариев;",
+    //         "Понимание принципов работы клиент-серверных приложений;",
+    //         "Проактивность.",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         "Опыт работы с SQL;",
+    //         "Опыт работы с Postman;",
+    //         "Работа с Jira/Confluence;",
+    //         "Знание английского языка.",
+    //       ],
+    //     },
+    //   },
+    // },
+    // UI1: {
+    //   icon: "./img/des-icon.svg",
+    //   position: "UI/UX Designer Senior",
+    //   experience: "4-6 лет",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Удаленное сотрудничество",
+    //   buttonData: "UI1",
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Обязанности:",
+    //       p: [
+    //         ";",
+    //         "."
+    //       ],
+    //     },
+    //     paragraph2: {
+    //       title: "Что ждем от кандидата:",
+    //       p: [
+    //         ";",
+    //         ".",
+    //       ],
+    //     },
+    //     paragraph3: {
+    //       title: "Будет плюсом:",
+    //       p: [
+    //         ";",
+    //         ";",
+    //         "."
+    //       ],
+    //     },
+    //   },
+    // },
+};
+
+let dataHide = {};
+dataHide = {
+    // QA2: {
+    //   icon: "./img/qa-icon.svg",
+    //   position: "QA Engineer Junior",
+    //   experience: "1-2 года",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "QA2",
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Нужно знать наш технологический стек, который включает в себя:",
+    //       p: ["UI/UX", "Figma", "Photoshop"],
+    //     }
+    //   },
+    // },
+    // FRONT2: {
+    //   icon: "./img/js-icon.svg",
+    //   position: "Front-end developer Junior",
+    //   experience: "1-2 года",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Удаленное сотрудничество",
+    //   buttonData: "FRONT2",
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Нужно знать наш технологический стек, который включает в себя:",
+    //       p: ["Js", "html/css", "Angular / Vue"],
+    //     },
+    //   },
+    // },
+    // UI2: {
+    //   icon: "./img/des-icon.svg",
+    //   position: "UI/UX Designer Middle",
+    //   experience: "4-6 лет",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "UI2",
+    //   inside: {
+    //     paragraph1: {
+    //       title: "Нужно знать наш технологический стек, который включает в себя:",
+    //       p: ["UI/UX", "Figma"],
+    //     },
+    //     paragraph2: {
+    //       title: " Title-2",
+    //       p: ["Lorem", "Photoshop"],
+    //     },
+    //   },
+    // },
+    // test: {
+    //   icon: "./img/des-icon.svg",
+    //   position: "test",
+    //   experience: "4-6 лет",
+    //   officeLocation: "Офис: г. Киев, Украина",
+    //   collaborationVariant: "Офис",
+    //   buttonData: "test",
+    //   inside: {
+    //     paragraph1: {
+    //       title: "test:",
+    //       p: ["UI/UX", "Figma"],
+    //     },
+    //     paragraph2: {
+    //       title: " Title-2",
+    //       p: ["Lorem", "Photoshop"],
+    //     },
+    //   },
+    // },
+};
